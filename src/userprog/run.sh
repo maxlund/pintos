c16425ae5090859b4e8890461783d8079bc4a959
Santiago Pagola <santipagola@gmail.com>
2018-01-25 09:07:50 +0100

    Change script

--- src/userprog/run.sh
+++ src/userprog/run.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-test -d build ||
-    {
-        echo "Build first" 2>&1
-        exit 1 
-    }
-
+# Clean
+make clean
+# Make
+make -j8
+# Cd into build
 cd build
-
+# Create disk
+pintos-mkdisk fs.dsk 2
+# Format it
+pintos --qemu -- -f -q
+# Copy our labtest
+pintos --qemu -p ../../examples/lab1test -a lab1test -- -q
+# Run labtest
 pintos --qemu -- run lab1test
-
