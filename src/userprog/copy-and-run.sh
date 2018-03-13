591f67d559755fd008dd8dc50007bcc7e6b9fa06
Santi Pagola <santipagola@gmail.com>
2018-03-13 16:05:09 +0100

    Fix paths

--- src/userprog/copy-and-run.sh
+++ src/userprog/copy-and-run.sh
@@ -19,11 +19,10 @@ USERPROG_TESTS="close-twice \
     multi-recurse \
     multi-child-fd"
 
-FILESYS_BASE=../../filesys
+FILESYS_BASE=../filesys
 
-TESTDIR_BASE=../../tests
-TESTDIR_FILESYS=$TESTDIR_BASE/filesys/base
-TESTDIR_USERPROG=$TESTDIR_BASE/userprog
+TESTDIR_FILESYS=$FILESYS_BASE/build/tests/filesys
+TESTDIR_USERPROG=tests/userprog
 
 # Make userprog and filesys first
 make -C $FILESYS_BASE -j8
@@ -32,12 +31,13 @@ make -j8
 # Go to the target directory
 cd build
 
+echo "PWD=$PWD"
 # Copy our labtest
 for test in $FILESYS_TESTS; do
-    pintos --qemu -p $TESTDIR_FILESYS/$test -a $test -- -q
+    echo pintos --qemu -p $TESTDIR_FILESYS/$test -a $test -- -q
 done
 
 for test in $USERPROG_TESTS; do
-    pintos --qemu -p $TESTDIR_USERPROG/$test -a $test -- -q
+    echo pintos --qemu -p $TESTDIR_USERPROG/$test -a $test -- -q
 done
 
