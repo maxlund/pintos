a7cfd312a33159b41f8f5118077c6382620d9b46
Santi Pagola <santipagola@gmail.com>
2018-03-13 15:52:54 +0100

    Update run script

--- src/userprog/copy-and-run.sh
+++ src/userprog/copy-and-run.sh
@@ -1,12 +1,43 @@
 #!/bin/bash
 
-# Make first
+FILESYS_TESTS="lg-create \
+    lg-full \
+    lg-random \
+    lg-seq-block \
+    lg-seq-random \
+    sm-create \
+    sm-full \
+    sm-random \
+    sm-seq-block \
+    sm-seq-random \
+    syn-read \
+    syn-remove \
+    syn-write"
+
+USERPROG_TESTS="close-twice \
+    read-normal \
+    multi-recurse \
+    multi-child-fd"
+
+FILESYS_BASE=../../filesys
+
+TESTDIR_BASE=../../tests
+TESTDIR_FILESYS=$TESTDIR_BASE/filesys/base
+TESTDIR_USERPROG=$TESTDIR_BASE/userprog
+
+# Make userprog and filesys first
+make -C $FILESYS_BASE -j8
 make -j8
+
 # Go to the target directory
 cd build
-# Build examples just in case
-make -C ../../examples
+
 # Copy our labtest
-pintos --qemu -p ../../examples/lab1test -a lab1test -- -q
-# Run labtest
-pintos --qemu -- run lab1test
+for test in $FILESYS_TESTS; do
+    pintos --qemu -p $TESTDIR_FILESYS/$test -a $test -- -q
+done
+
+for test in $USERPROG_TESTS; do
+    pintos --qemu -p $TESTDIR_USERPROG/$test -a $test -- -q
+done
+
